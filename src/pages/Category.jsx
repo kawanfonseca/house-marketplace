2e25aa75bab1ec8f6b39b6ffa9a97d77f59acc52
Kawan Fonseca <kawanfonseca@hotmail.com>
2022-05-11 11:20:05 -0300

    Finished creating listing with firebase connection

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -14,7 +14,7 @@ import { toast } from 'react-toastify'
 import Spinner from '../components/Spinner'
 import ListingItem from '../components/ListingItem'
 
-function Offers() {
+function Category() {
     const [listings, setListings] = useState(null)
     const [loading, setLoading] = useState(true)
 
@@ -30,7 +30,7 @@ function Offers() {
                 // Create a query
                 const q = query(
                     listingsRef,
-                    where('offer', '==', true),
+                    where('type', '==', params.categoryName),
                     orderBy('timestamp', 'desc'),
                     limit(10)
                 )
@@ -55,13 +55,13 @@ function Offers() {
         }
 
         fetchListings()
-    }, [])
+    }, [params.categoryName])
 
     return (
         <div className="category">
             <header>
                 <p className="pageHeader">
-                    Offers
+                    {params.categoryName === 'rent' ? 'Places for rent' : 'Places for sale'}
                 </p>
             </header>
 
@@ -79,10 +79,10 @@ function Offers() {
                         </ul>
                     </main>
                 </> :
-                <p>There are not current offers</p>
+                <p>No listings for {params.categoryName}</p>
             }
         </div>
     )
 }
 
-export default Offers
+export default Category
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,11 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { getAuth, updateProfile } from 'firebase/auth'
 import { updateDoc, doc } from 'firebase/firestore'
 import { db } from '../firebase.config'
 import { useNavigate, Link } from 'react-router-dom'
 import { toast } from 'react-toastify'
+import arrowRight from '../assets/svg/keyboardArrowRightIcon.svg'
+import homeIcon from '../assets/svg/homeIcon.svg'
 
 function Profile() {
     const auth = getAuth()
@@ -67,6 +69,11 @@ function Profile() {
                     <input type="text" id="email" className={!changeDetails ? "profileEmail" : "profileEmailActive"} disabled={!changeDetails} value={email} onChange={onChange} />
                 </form>
             </div>
+            <Link to="/create-listing" className="createListing">
+                <img src={homeIcon} alt="home" />
+                <p>Sell or rent your home</p>
+                <img src={arrowRight} alt="arrow right" />
+            </Link>
         </main>
     </div>
 }
